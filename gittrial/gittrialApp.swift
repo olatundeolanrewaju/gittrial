b587afc3155d0a7ce1ceffe2044c67e211d6e38e
olatundeolanrewaju <momo@olatunde.dev>
2024-12-04 22:34:17 +0000

    Initial Commit

---
+++ gittrial/gittrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittrialApp.swift
+//  gittrial
+//
+//  Created by Olanrewaju Olatunde on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct gittrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
